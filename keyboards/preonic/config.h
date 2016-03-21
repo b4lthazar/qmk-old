f807e1273de8c1df1b8fee7ba74a13d4b6877633
b4lthazar <b4lthazar@users.noreply.github.com>
2016-03-20 19:59:28 -0700

    My keymap

--- keyboards/preonic/config.h
+++ keyboards/preonic/config.h
@@ -89,4 +89,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+
+#define TAPPING_TOGGLE  1
+#define TAPPING_TERM    100
+
+
 #endif

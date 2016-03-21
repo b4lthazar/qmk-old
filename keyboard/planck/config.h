69063828880f72c977ccb9fbcee9846f5d3f18dd
b4lthazar <b4lthazar@users.noreply.github.com>
2016-03-20 19:59:28 -0700

    My keymap

--- keyboard/planck/config.h
+++ keyboard/planck/config.h
@@ -85,4 +85,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+
+#define TAPPING_TOGGLE  1
+#define TAPPING_TERM    100
+
+
 #endif

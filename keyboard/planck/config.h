74e2dd0ba72ba25d500d599c266510697e181677
b4lthazar <b4lthazar@users.noreply.github.com>
2016-03-22 07:23:17 -0700

    Increased tapping toggle to 2 because I was accidentally triggering going to layers

--- keyboard/planck/config.h
+++ keyboard/planck/config.h
@@ -86,7 +86,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_FUNCTION
 
 
-#define TAPPING_TOGGLE  1
+#define TAPPING_TOGGLE  2
 #define TAPPING_TERM    100
 
 
